df79a81cfd0944167972a259f509a8d6b7f03c38
Golubkov Mikhail Andreev <creepygolubkov@gmail.com>
2025-11-03 21:41:59 +0200

    16: statistics service covered with tests

---
+++ week1/nhl-statistics/src/tests/statistics_service_test.py
@@ -0,0 +1,33 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),  #  4+12 = 16
+            Player("Lemieux", "PIT", 45, 54), # 45+54 = 99
+            Player("Kurri",   "EDM", 37, 53), # 37+53 = 90
+            Player("Yzerman", "DET", 42, 56), # 42+56 = 98
+            Player("Gretzky", "EDM", 35, 89)  # 35+89 = 124
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+  def setUp(self):
+    self.service = StatisticsService(PlayerReaderStub())
+
+  def test_search(self):
+     res: Player = self.service.search("Kurri")
+     self.assertEqual(res.name, "Kurri")
+     res: Player = self.service.search("James")
+     self.assertEqual(res, None)
+
+  def test_team(self):
+     res: list[Player] = self.service.team("EDM")
+     self.assertEqual(len(res), 3)
+
+  def test_top(self):
+     res: list[Player] = self.service.top(4)
+     names = list(map(lambda x: x.name, res))
+     self.assertEqual(names, ["Gretzky", "Lemieux", "Yzerman", "Kurri"])
+  
